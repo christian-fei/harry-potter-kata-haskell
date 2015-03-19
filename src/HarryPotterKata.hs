86a3bd1b3acf34517fa1f1566efde6c3174cddf1
Christian Fei <crifei93@gmail.com>
2015-03-19 14:45:28 +0100

    added priceforBooks with 0 and recurisive amount of books

--- src/HarryPotterKata.hs
+++ src/HarryPotterKata.hs
@@ -1,5 +1,11 @@
-module HarryPotterKata
+module HarryPotterKata (
+  priceForBooks
+  )
 where
 
 priceForBooks :: [Int] -> Float
-priceForBooks bookList = 0.0
+priceForBooks books = priceForBooksRecursive books 0.0
+
+priceForBooksRecursive :: [Int] -> Float -> Float
+priceForBooksRecursive [] acc = acc
+priceForBooksRecursive (book:rest) acc = priceForBooksRecursive rest (acc + 8.0)
--- src/HarryPotterKata.test.hs
+++ src/HarryPotterKata.test.hs
@@ -10,7 +10,10 @@ import HarryPotterKata
 main = defaultMain tests
 
 tests = hUnitTestToTests $ TestList $
-  [noBookCosts0Euro]
+  [noBookCosts0Euro, oneBookCosts8Euro]
 
 noBookCosts0Euro =
   priceForBooks [] ~?= 0.0
+
+oneBookCosts8Euro =
+  priceForBooks [1] ~?= 8.0
